a39361886ef5f2a8d6331db464c62fea73236025
Ariel Wainer <ari@ariwainer.com.ar>
2015-06-08 07:27:00 -0300

    Implements lookup and some formatting.

--- lili.lisp
+++ lili.lisp
@@ -1,15 +1,30 @@
+
+
+(defun lookup (elem l)
+  (if (null l) nil
+    (if (eq elem (caar l))
+      (cdar l)
+      (lookup elem (cdr l))
+    )
+  )
+)
+
+
 ; TODO
-(defun lookup nil)
 (defun expand_ambient nil)
+
+
+
 
 (defun tclapply (fn lea amb)
     (if (atom fn)
         (cond
-            ((eq fn 'cons)  (cons (car lea) (cadr lea)))
+            ((eq fn 'cons)   (cons (car lea) (cadr lea)))
             ((eq fn  'car)   (caar lea))
             ;TODO: aritmetic, not, functional forms, user defined functions
             (t              (tclapply   (lookup fn amb) lea amb))
         )
+    ; lambda
     (tcleval    (caddr fn)  (expand_ambient (cadr fn)  lea amb) )
 )
 
@@ -18,31 +33,32 @@
         nil
         ; Expresiones atómicas
         (if (atom exp)
-            ; Numericas
-            (if (numberp exp)
-            exp
-            (lookup exp amb)
-            (cond
-                ((eq (car exp) 'quoute) (cadr exp))
-                ((eq (car exp) 'if)     (if (tcleval  (cadr exp)  amb)
-                                            (tcleval  (caddr exp) amb)
-                                            (tcleval  (caddr exp) amb)
-                                        )
-                )
-                ; TODO
-                (cond nil)
-                ((eq (car exp) 'lambda) exp)
-                ((eq (car exp) 'and )   (if (null (tcleval (cadr exp) amb))
-                                            nil
-                                            (tcleval (caddr exp) amb)
-                                        )
-                )
-                (eq (car exp)   'or)    (if (null (tcleval (cadr exp) amb))
-                                            (tcleval (caddr) amb)
-                                            t
-                                        )
-                (t    (tclapply  (car exp)  (mapcar (lambda (x) (tcleval x amb)) (cdr exp))))
-            )
+            ; Numericas o definidas en el ambiente
+            (if (numberp exp)  exp (lookup exp amb))
+ 	    (
+		; Expresiones no atómicas
+	        (cond
+	            ((eq (car exp) 'quoute) (cadr exp))
+	            ((eq (car exp) 'if)     (if (tcleval  (cadr exp)  amb)
+	                                        (tcleval  (caddr exp) amb)
+	                                        (tcleval  (caddr exp) amb)
+	                                    )
+	            )
+	            ; TODO
+	            (cond nil)
+	            ((eq (car exp) 'lambda) exp)
+	            ((eq (car exp) 'and )   (if (null (tcleval (cadr exp) amb))
+	                                        nil
+	                                        (tcleval (caddr exp) amb)
+	                                    )
+	            )
+	            (eq (car exp)   'or)    (if (null (tcleval (cadr exp) amb))
+	                                        (tcleval (caddr) amb)
+	                                        t
+	                                    )
+            	    ((eq fn 'not)    (not (tcleval lea amb)))   
+	            (t    (tclapply  (car exp)  (mapcar (lambda (x) (tcleval x amb)) (cdr exp))))
+	        )
 
             )
         )
