ee6b95c327b4ccbf21110bc61ad115891cc7b8a9
Ariel Wainer <ari@ariwainer.com.ar>
2015-06-25 22:11:14 -0300

    Initial steps on evaluar.

--- c.lisp
+++ c.lisp
@@ -1,3 +1,12 @@
+(defun belongs (x l)
+  (cond
+    ((null l      ) nil)
+    ((eq x (car l))   t)
+    (t      (belongs x (cdr l)))
+  ) 
+) 
+
+
 
 (defun agregar (defs mem)
   (cond
@@ -16,6 +25,91 @@
     )
   )
 )
+
+(defun tobool (x) (not (eq 0 x)))
+
+(defun esoperador (x)
+    (belongs x  '(+ - * / < > <= >= ++ -- || &&) )
+)
+
+(defun operar (operador operando1 operando2)
+    (cond
+        ((belongs operador '(+ - * /))  (apply operador (list operando1 operando2)))
+        (t nil) ;TODO 
+    )
+)
+
+(defun peso (x)
+    (case x 
+        ('+ 2)
+        ('- 2)
+        ('* 3)
+        ('/ 3)
+    )
+)
+;(print (operar '* 2 3))
+
+(defun evaluar (expr mem &optional (operadores nil) (operandos nil))
+    (if (esoperador (car expr))
+        ;vino un operador y no tengo ninguno
+        (if (null operadores)
+            (evaluar mem (cdr expr)  (cons (car expr) operadores) operandos)
+            ;vino un operador pero ya tenia uno
+            (if (> (peso (car expr)) (peso (cdr operadores)))
+                ;la que viene tiene mayor prioridad que la que estab en la pila
+                (evaluar mem (cdr expr)  (cons (car expr) operadores) operandos)
+                ;la que viene tiene menor prioridad
+            )
+        )
+        
+        ; No es un operador, lo apilo
+        (evaluar mem (cdr expr)  operadores (cons (car expr) operandos))
+    )
+)
+
+(trace evaluar)
+(evaluar nil '( 2 + 3 * 4))
+;(defun evaluar (exp mem &optional (operadores nil) (operandos nil))
+;   (if (null exp)
+;     (if (null operadores) 
+;       (car operandos) ; fin de la expresion
+;       (evaluar 
+;         nil 
+;         mem 
+;         (cdr operadores)
+;         (cons (operar (car operadores) (cadr operandos) (car operandos)) (cddr operandos))
+;       )
+;     )
+;     ;exp not null
+;     (if (esoperador (car exp))
+;       (if (null operadores)
+;         (evaluar (cdr exp)
+;                  mem
+;                  (cons (car exp) operadores) 
+;                  operandos
+;         )
+;         (if (> (peso (car exp) (peso car operadores)))
+;            (evaluar (cdr exp) mem (cons (car exp) operadores) operandos)
+;            (evaluar 
+;              nil 
+;              mem 
+;              (cdr operadores)
+;              (cons (operar (car operadores) (cadr operandos) (car operandos)) (cddr operandos))
+;            )
+;        )
+;      )
+;      (
+;        evaluar operadores (cons (car exp) operandos )
+;      )
+;    )
+;  )
+;)
+
+(trace evaluar)
+(trace operar)
+(print (evaluar '(3 + 4 + 5) nil))
+
+
 
 (defun run (pgm ent &optional (mem nil))
   (if   (null pgm)  nil
@@ -37,13 +131,13 @@
     )
 )
 
-(defun belongs (x l)
-  (cond
-    ((null l      ) nil)
-    ((eq x (car l))   t)
-    (t      (belongs x (cdr l)))
-  ) 
-) 
+
+
+
+
+
+
+
 
 (defun scanf (name mem val)
     (cond
@@ -53,8 +147,6 @@
     )
 )
 
-(defun suma nil)
-(trace suma)
 ;(trace while)
 
 (defun ejec (pgm ent mem &optional (sal nil))
@@ -82,11 +174,11 @@
             )
 
 
-            ((belongs  (cadar pgm) '(+ - * / < > <= >=))    (apply (cadar pgm) 
-                                                                (list  (ejec  (list(caar pgm)) ent mem sal)
-                                                                        (ejec (list(caddar pgm)) ent mem sal))
-                                                            )
-            )
+            ;((belongs  (cadar pgm) '(+ - * / < > <= >=))    (apply (cadar pgm) 
+            ;                                                    (list  (ejec  (list(caar pgm)) ent mem sal)
+            ;                                                            (ejec (list(caddar pgm)) ent mem sal))
+            ;                                                )
+            ;)
 
             ((eq (caar pgm) 'while)    (if  (ejec (list (cadar pgm)) ent mem sal)
                                          (ejec (append (caddar pgm) pgm) ent mem sal) ; appendeo al cuerpo el bloque del while
@@ -138,4 +230,4 @@
 (trace scanf)
 (trace run)
 ;(run pgm2 ent1)
-(run pgm2 nil)
+;(run pgm2 nil)
