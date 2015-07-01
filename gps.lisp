7e6758ac69fa3327abb4911c31cfecd8a56f2a13
Ariel Wainer <awainer@contentamobile.com>
2015-07-01 12:42:51 -0300

    Broken gps.

--- gps.lisp
+++ gps.lisp
@@ -41,20 +41,31 @@
   ) 
 )
 
-(trace belongs)
+;(trace belongs)
 
-(defun vecinos (nodo grafo &optional (res nil) ) 
+;(defun vecinos (nodo grafo &optional (res nil) ) 
+;    (if (null grafo)
+;        res
+;        (if (belongs nodo (cadar grafo))
+;            (vecinos nodo (cdr grafo) (cons (caar grafo) res))
+;            (vecinos nodo (cdr grafo)   res)
+;        )
+;    )
+;)
+
+(defun vecinos (nodo grafo)
     (if (null grafo)
-        res
-        (if (belongs nodo (cadar grafo))
-            (vecinos nodo (cdr grafo) (cons (caar grafo) res))
-            (vecinos nodo (cdr grafo)   res)
+        nil
+        (if (eq nodo (caar grafo))
+            (cadar grafo)
+            (vecinos nodo (cdr grafo))
         )
     )
 )
             
 ;(trace vecinos)
-;(print (vecinos 10 grafo))
+;(print (vecinos 29 grafo))
+
 (defun elimina_elem (x l)
     (if (null l)
         nil
@@ -79,22 +90,80 @@
 
 ;(print (diferencia '(5 2 9 2 6 3 1 5) '(1 2)))
 
-(defun gps (i f grafo &optional (tray (list (list i))))
-    (if (null tray)
-      nil
-      (if (eq (caar tray) f)
-        (reverse tray)
-        (gps i f grafo
-             (append
-               (mapcar (lambda (x) (cons x (car tray)))
-                 (diferencia  (vecinos    (caar tray) grafo)
-                              (car tray)
-                 )
-               )
-             )
-        )
-     )
-   )
+(defun expand_tray (tray  grafo)
+    (mapcar
+        (lambda (x) (cons x tray))
+        (vecinos_sin_visitar tray grafo)
+    )
 )
 
+(defun vecinos_sin_visitar (tray grafo)
+        (diferencia (vecinos (car tray) grafo)   tray)
+)
+
+(trace expand_tray)
+
+(defun gps (i f grafo &optional (tray (list (list i))))
+nil)
+;(defun gps (i f grafo &optional (tray (list (list i))))
+;
+;    (mapcar
+;     (lambda (x) (gps i f grafo (expand_tray x grafo)))
+;     tray   
+;    )     
+;    (if (or (eq f (caar tray)) (null (vecinos_sin_visitar ( ))  )
+    
+;        (cons (car tray) (gps i f grafo (cdr tray)))
+        ;(cons (car tray) (gps i f grafo (cdr tray)))
+;        (gps i f grafo
+;            (append 
+;                (expand_tray (car tray) grafo)
+;                (cdr tray)
+;            )
+;        )
+;    )
+;    (if (null tray)
+;      nil
+;    )
+)
+;      (if (eq (caar tray) f)
+;        (reverse tray)
+;
+;         (gps i f grafo
+;         (mapcar 
+;              (lambda (x) (expand_tray x grafo))
+;              tray
+;         )
+;         )
+;        (mapcar (lambda (x) 
+;                    ;(append  (vecinos (car x) grafo) x)        
+;                       (mapcar 'list
+;                            (diferencia  (vecinos    (car x) grafo)
+;                                         x
+;                            )
+;                            x
+;                       )
+;                )
+;                tray
+;        )
+
+            
+       ;(vecinos (caar tray) grafo)
+       ; (gps i f grafo
+       ;      (append
+             ;  (mapcar (lambda (x) (cons
+             ;          )
+                      ; (diferencia  (vecinos    (caar tray) grafo)
+                      ;              (car tray)
+                      ; )
+             ;  )
+       ;      )
+       ; )
+;     )
+;   )
+;)
+
+(trace gps)
 (print (gps 10 21 grafo))
+;(EXPAND_TRAY (car (EXPAND_TRAY  '(16 10)  grafo)) grafo)
+
